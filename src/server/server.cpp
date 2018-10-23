2044a1973f994b5045b5d029216ab917fd961bc2
Zachary Lund <zachary.lund@streamlabs.com>
2018-10-23 14:32:32 -0700

    Fix up server to allow N sessions and N threads

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -5,150 +5,247 @@
 
 namespace ipc {
 
-asio::io_service g_service;
-
 /*
-	Note that when we call async_read, 
-	the function will not return until a 
-	condition has completed. By default, 
-	it's whenever the buffer is full. 
+	Note that when we call async_read,
+	the function will not return until a
+	condition has completed. By default,
+	it's whenever the buffer is full.
 
 	This means that if our client only gives
-	10 bytes of data at a time, it will not 
-	return until that happens 100 times. 
+	10 bytes of data at a time, it will not
+	return until that happens 100 times.
  */
-const int amount_buffered = 14;
 
-namespace server {
+/******************************************************************************
+ * The individual server. Represents each connection to the server.
+ *****************************************************************************/
+template <size_t BufferSize = 4096>
+class server {
+	struct session {
+		session(asio::io_context &io_ctx, HANDLE handle)
+		: stream(io_ctx, handle)
+		{}
 
-class session {
+		asio::windows::stream_handle stream;
+
+		uint8_t read_buffer[BufferSize] { 0 };
+		uint8_t write_buffer[BufferSize] { 0 };
+	};
+
+	const char *name;
+	asio::io_context io_ctx;
+	std::list<session> sessions;
+
+	void handle_connection(
+	  session &session,
+	  const asio::error_code &e,
+	  std::size_t bt
+	);
+
+	void handle_async_read(
+	  session &session,
+	  asio::error_code error,
+	  std::size_t length
+	);
+
+	void listen(session &session);
+
 public:
-	session(asio::io_service &service, HANDLE pipe) 
-	: stream(service, pipe), work(service), service(service)
-	{
-	}
+	explicit server(const char *name);
+	~server();
 
-	void read() 
-	{
-		asio::async_read(
-			stream,
-			asio::mutable_buffer(read_buffer, amount_buffered), 
-			[this](asio::error_code error, std::size_t length) {
-				if (error) {
-					printf("Failed to read from pipe: %s\n", error.message().c_str());
+	/* Each connection is the same so no parameters
+	 * are allowed here. */
 
-					if (error.value() == ERROR_BROKEN_PIPE) {
-						printf("Client closed the handle.\n");
-						BOOL okay = DisconnectNamedPipe(stream.native_handle());
+	void add_session();
+	void listen();
 
-						if (!okay) {
-							printf("Failed to reset pipe!\n");
-							service.stop();
-						}
+	/* Remove session won't be removed until
+	 * it's finished its current job. */
+	void remove_session();
 
-						start();
-					}
+	/* Signal each session to close */
+	void close();
 
-					return;
-				}
-
-				printf("Message Read: %i: %.*s\n", length, length, read_buffer);
-				read();
-			}
-		);
-	}
-
-	void handle_connection(const asio::error_code &error, std::size_t bytes_transferred)
-	{
-		printf("Overlapped Event: %s\n", error.message().c_str());
-		
-		read();
-	}
-
-	void start() 
-	{
-		asio::windows::overlapped_ptr overlapped(
-			service,
-			[this](const asio::error_code &e, std::size_t bt) {
-				this->handle_connection(e, bt);
-			}
-		);
-
-		BOOL connected = ConnectNamedPipe(stream.native_handle(), overlapped.get());
-		DWORD connect_error = GetLastError();
-		
-		if (!connected && connect_error != ERROR_IO_PENDING)
-		{
-			asio::error_code ec(connect_error, asio::error::get_system_category());
-			overlapped.complete(ec, 0);
-		}
-		else
-		{
-			printf("Pending...\n");
-			overlapped.release();
-		}
-	}
-
-private:
-	uint8_t read_buffer[4096];
-	uint8_t write_buffer[4096];
-
-	asio::io_service &service;
-	asio::io_service::work work;
-	asio::windows::stream_handle stream;
+	/* Execute handlers on this thread */
+	void run();
 };
 
+template <size_t BufferSize>
+server<BufferSize>::server(const char *name)
+: name(name)
+{
 }
 
+template <size_t BufferSize>
+server<BufferSize>::~server()
+{
 }
 
-/* 
+template <size_t BufferSize>
+void server<BufferSize>::run()
+{
+	io_ctx.run();
+}
+
+template <size_t BufferSize>
+void server<BufferSize>::add_session()
+{
+	const DWORD dwOpenMode =
+		PIPE_ACCESS_DUPLEX |
+		FILE_FLAG_OVERLAPPED;
+
+	const DWORD dwPipeMode =
+		PIPE_TYPE_BYTE |
+		PIPE_READMODE_BYTE |
+		PIPE_WAIT;
+
+	/* A reasonable guess */
+	const DWORD dwTimeout = 0;
+
+	/* Not used */
+	LPSECURITY_ATTRIBUTES lpSecurityAttributes = NULL;
+
+	HANDLE hServerPipe = CreateNamedPipe(
+		name,
+		dwOpenMode,
+		dwPipeMode,
+		PIPE_UNLIMITED_INSTANCES,
+		BufferSize /* Out Buffer */,
+		BufferSize /* In Buffer */,
+		dwTimeout,
+		lpSecurityAttributes
+	);
+
+	sessions.emplace_back(io_ctx, hServerPipe);
+}
+
+template <size_t BufferSize>
+void server<BufferSize>::handle_async_read(
+  server::session &session,
+  asio::error_code error,
+  std::size_t length
+) {
+	if (error) {
+		printf("Failed to read from pipe: %s\n",
+		       error.message().c_str());
+
+		if (error.value() != ERROR_BROKEN_PIPE) return;
+
+		printf("Client closed the handle.\n");
+		BOOL okay = DisconnectNamedPipe(session.stream.native_handle());
+
+		if (!okay) {
+			printf("Failed to reset pipe!\n");
+			io_ctx.stop();
+		}
+
+		session.stream.close();
+
+		return;
+	}
+
+	printf("Message Read: %i: %.*s\n", length, length, session.read_buffer);
+
+	session.stream.async_read_some(
+		asio::mutable_buffer(session.read_buffer, BufferSize),
+		[this, &session](asio::error_code error, std::size_t length) {
+			this->handle_async_read(session, error, length);
+		}
+	);
+}
+
+template <size_t BufferSize>
+void server<BufferSize>::handle_connection(
+  server::session &session,
+  const asio::error_code &e,
+  std::size_t bt
+) {
+	printf("Overlapped Event: %s\n", e.message().c_str());
+
+	add_session();
+	listen(sessions.back());
+
+	session.stream.async_read_some(
+		asio::mutable_buffer(session.read_buffer, BufferSize),
+		[this, &session](asio::error_code error, std::size_t length) {
+			handle_async_read(session, error, length);
+		}
+	);
+}
+
+template <size_t BufferSize>
+void server<BufferSize>::listen(server::session &session)
+{
+	asio::windows::overlapped_ptr overlapped(
+		io_ctx,
+		[this, &session](const asio::error_code &e, std::size_t bt) {
+			handle_connection(session, e, bt);
+		}
+	);
+
+	BOOL connected = ConnectNamedPipe(
+		session.stream.native_handle(), overlapped.get());
+
+	DWORD connect_error = GetLastError();
+
+	if (!connected && connect_error != ERROR_IO_PENDING) {
+		asio::error_code ec(
+			connect_error,
+			asio::error::get_system_category()
+		);
+		overlapped.complete(ec, 0);
+	} else {
+		printf("Pending...\n");
+		overlapped.release();
+	}
+}
+
+template <size_t BufferSize>
+void server<BufferSize>::listen()
+{
+	for (auto &session : sessions) {
+		listen(session);
+	}
+}
+
+} // namespace ipc
+
+/*
  * I would like to think this code as a guide:
   * https://svn.boost.org/svn/boost/sandbox/process/boost/process/detail/pipe.hpp
  */
 
 int main(int argc, char *argv[])
 {
-	const DWORD dwOpenMode =
-		PIPE_ACCESS_DUPLEX |
-		FILE_FLAG_OVERLAPPED;
+	/* There's a big difference between the number of sessions
+	 * and the number of threads. You can have one thread but
+	 * a ton of sessions. However, it's somewhat pointless to
+	 * have multiple threads but only one session. It doesn't
+	 * scale well. There's a golden area here as a result.*/
 
-	const DWORD dwPipeMode = 
-		PIPE_TYPE_BYTE |
-		PIPE_READMODE_BYTE |
-		PIPE_WAIT;
+	/* Number of simultaneous listeners. Notice the actual number
+	 * of sessions will dynamically increase */
+	const char *pipe_name = "\\\\.\\pipe\\bobby";
+	const int num_sessions = 1;
+	const int num_threads = 2;
+	std::vector<std::thread> threads;
 
-	/* A reasonable guess */
-	const DWORD dwPipeSize = 32768;
-	const DWORD dwTimeout = 0;
+	ipc::server<4096> server(pipe_name);
 
-	/* Not used */
-	LPSECURITY_ATTRIBUTES lpSecurityAttributes = NULL;
+	for (int i = 0; i < num_sessions; ++i)
+		server.add_session();
 
-	ipc::server::session *sessions[10];
+	/* Give the server work so it doesn't immediately quit */
+	server.listen();
 
-	for (int i = 0; i < 10; ++i) {
-		HANDLE hServerPipe = CreateNamedPipe(
-			"\\\\.\\pipe\\bobby",
-			dwOpenMode,
-			dwPipeMode,
-			PIPE_UNLIMITED_INSTANCES,
-			dwPipeSize /* Out Buffer */, 
-			dwPipeSize /* In Buffer */,
-			dwTimeout,
-			lpSecurityAttributes
-		);
-
-		if (hServerPipe == INVALID_HANDLE_VALUE) {
-			printf("Failed to create pipe!");
-			return 0;
-		}
-
-		sessions[i] = new ipc::server::session(ipc::g_service, hServerPipe);
-		sessions[i]->start();
+	for (int i = 0; i < num_threads; ++i) {
+		threads.emplace_back([&server] () {
+			server.run();
+		});
 	}
 
-	ipc::g_service.run();
-
-	return 0;
+	for (auto &thread : threads) {
+		thread.join();
+	}
 }
